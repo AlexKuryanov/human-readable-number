9ff56ec97a2634caa34451d4ccb77bf79c6bc01d
AlexKuryanov <lexxus07@yandex.ru>
2021-03-15 20:47:03 +0300

    refactor: change formatting

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
   let units = [
-    '', 'one', 'two', 'three', 'four', 'five', 
+    '', 'one', 'two', 'three', 'four', 'five',
     'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
     'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
-  ]   
+  ]
   let dozens = [
     '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
     'seventy', 'eighty', 'ninety'
   ]
   let hundred = 'hundred';
-  
+
   let result = '';
   number = String(number);
   if (number == 0) {
@@ -20,33 +20,33 @@ module.exports = function toReadable (number) {
     result = units[number]
     return result.trim();
   }
-  
+
   //numbers from 20 to 99
-if (number.length === 2) {
+  if (number.length === 2) {
     result = dozens[number.charAt(0)] + ' ';
     result += units[number.charAt(1)];
     return result.trim();
   }
 
   //numbers from 100 to 999
-  
-    if (number.length === 3 && number.charAt(1) == 1) {     
-        result = units[number.charAt(0)] + ' ' + hundred + ' ';
-       result += units[number.slice(1)];
-        return result.trim();
 
-    } 
-    else if (number.length === 3 && number.charAt(1) == 0) {
-      result = units[number.charAt(0)] + ' ' + hundred + ' ';
-        result += dozens[number.charAt(1)];
-        result += units[number.charAt(2)];
-      return result.trim();
+  if (number.length === 3 && number.charAt(1) == 1) {
+    result = units[number.charAt(0)] + ' ' + hundred + ' ';
+    result += units[number.slice(1)];
+    return result.trim();
 
-    } 
-    else {
-      result = units[number.charAt(0)] + ' ' + hundred + ' ';
-      result += dozens[number.charAt(1)] + ' ';
-      result += units[number.charAt(2)];
-      return result.trim();
-    }
+  }
+  else if (number.length === 3 && number.charAt(1) == 0) {
+    result = units[number.charAt(0)] + ' ' + hundred + ' ';
+    result += dozens[number.charAt(1)];
+    result += units[number.charAt(2)];
+    return result.trim();
+
+  }
+  else {
+    result = units[number.charAt(0)] + ' ' + hundred + ' ';
+    result += dozens[number.charAt(1)] + ' ';
+    result += units[number.charAt(2)];
+    return result.trim();
+  }
 }
